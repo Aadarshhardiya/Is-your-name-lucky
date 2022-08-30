d99c17a47a00f2125e885573c6670757856fcc31
aadarshhardiya <aadarsh.hardiya@gmail.com>
2022-08-30 21:35:37 +0530

    merge-up

---
+++ script.js
@@ -0,0 +1,33 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+
+function compareValues(sum, luckyNumber) {
+    if(sum%luckyNumber===0) {
+        outputBox.innerText="🤩!! Your birthday is lucky";   
+    }   else{
+        outputBox.innerText="oops!! your birthday is not lucky 😬"
+    }
+}
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum&&luckyNumber.value)
+    compareValues(sum, luckyNumber.value)
+    else
+    outputBox.innerText="Please enter both the fields";
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-","");
+    let sum =0;
+    for(let i=0; i<dob.length; i++) {
+        sum= sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky)
